25b949f47f80b53721455d80b40f96c6c516f140
Kelgors <github.hurt819@passmail.net>
2026-05-08 12:56:48 +0000

    fix formatting

--- packages/runner-github/src/index.ts
+++ packages/runner-github/src/index.ts
@@ -3,24 +3,24 @@ import { env } from "./env.js";
 import { GithubWatchJob } from "./github.js";
 
 const octokit = new Octokit({
-	auth: env.GITHUB_TOKEN,
+  auth: env.GITHUB_TOKEN,
 });
 
 let timeoutId: NodeJS.Timeout | null = null;
 function loop() {
-	timeoutId = setTimeout(
-		async () => {
-			await new GithubWatchJob(octokit).run();
-			loop();
-		},
-		env.INTERVAL_MIN * 60 * 1000,
-	);
+  timeoutId = setTimeout(
+    async () => {
+      await new GithubWatchJob(octokit).run();
+      loop();
+    },
+    env.INTERVAL_MIN * 60 * 1000,
+  );
 }
 
 function onExit() {
-	console.log("Shutting down...");
-	if (timeoutId) clearTimeout(timeoutId);
-	process.exit(0);
+  console.log("Shutting down...");
+  if (timeoutId) clearTimeout(timeoutId);
+  process.exit(0);
 }
 process.on("SIGINT", onExit);
 process.on("SIGTERM", onExit);
--- packages/runner-github/src/prisma.ts
+++ packages/runner-github/src/prisma.ts
@@ -5,11 +5,11 @@ import { env } from "./env.js";
 const uri = new URL(`${env.DATABASE_URL}`);
 
 const adapter = new PrismaPg({
-	host: uri.hostname,
-	port: parseInt(uri.port, 10) || 5432,
-	user: uri.username,
-	password: uri.password,
-	database: uri.pathname.slice(1),
+  host: uri.hostname,
+  port: parseInt(uri.port, 10) || 5432,
+  user: uri.username,
+  password: uri.password,
+  database: uri.pathname.slice(1),
 });
 const prisma = new PrismaClient({ adapter });
 
